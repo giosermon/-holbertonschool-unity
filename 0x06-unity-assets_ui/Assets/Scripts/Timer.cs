67cd65355f3f5fa6312e7e5688468e22c20cb283
giosermon <2172@holbertonschool.com>
2022-02-23 20:44:29 -0500

    update-all task working

---
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public Text finalTimeText;
+    float timer;
+    float milliseconds;
+    float seconds;
+    float minutes;
+    bool timerActive = true;
+
+    void Start()
+    {
+        timer = 0;
+    }
+
+    void Update()
+    {
+        if (timerActive)
+            StopWatchCalcul();
+    }
+
+    void StopWatchCalcul()
+    {
+        timer += Time.deltaTime;
+        minutes = (int)(timer / 60);
+        seconds = (int)(timer % 60);
+        milliseconds = (int)((timer - seconds) * 100) % 100;
+        timerText.text = $"{minutes.ToString("0")}:{seconds.ToString("00")}:{milliseconds.ToString("00")}";
+    }
+
+    public void StopTimer()
+    {
+        timerActive = false;
+        timerText.color = Color.green;
+        timerText.fontSize = 60;
+    }
+
+    public void Win()
+    {
+        finalTimeText.text = timerText.text;
+        timerText.gameObject.SetActive(false);
+        FindObjectOfType<CameraController>().CameraDisabled = true;
+        FindObjectOfType<PlayerController>().enabled = false;
+        Cursor.visible = true;
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+	public GameObject winCanvas;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.name == "Player")
+		{
+			other.GetComponent<Timer>().StopTimer();
+			winCanvas.SetActive(true);
+			FindObjectOfType<Timer>().Win();
+		}
+	}
+}
