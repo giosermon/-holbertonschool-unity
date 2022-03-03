e2821d181d0a3e19c70b042f736b87d43b9fc9d6
giosermon <2172@holbertonschool.com>
2022-03-03 18:12:18 -0500

    animations,keyframe,transitions

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject timerCanvas;
+    public PlayerController playerController;
+    
+    public void EndOfIntro()
+    {
+        mainCamera.SetActive(true);
+        timerCanvas.SetActive(true);
+        playerController.enabled = true;
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ 0x07-unity-animation/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+	public GameObject winCanvas;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.name != "Player") return;
+		
+		var timer = other.GetComponent<Timer>();
+		timer.StopTimer();
+		winCanvas.SetActive(true);
+		timer.Win();
+	}
+}
